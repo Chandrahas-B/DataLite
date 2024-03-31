8b64be1a33233aa71478adb0f860f82aa8663706
Vineel <vineelakash03@gmail.com>
2024-03-31 23:32:52 +0530

    Updated Code to Include Audio Files

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ from app_funcs import *
 import pandas as pd
 import torch
 import gc
+import matplotlib.pyplot as plt
+import librosa
+from skimage.metrics import peak_signal_noise_ratio as psnr
+from skimage.metrics import structural_similarity as ssim
+import numpy as np
+from skimage.metrics import mean_squared_error as MSE
+
+
 
 pd.set_option('display.float_format', '{:.2f}'.format)
 st.set_page_config(
@@ -27,10 +35,12 @@ uploaded_file = st.file_uploader("Upload File 🚀", type=["png","jpg","jpeg",'w
 image_ext = ["png","jpg","jpeg"]
 audio_ext = ['wav','mp3']
 
-try:
-    if uploaded_file is not None:
+# try:
+if uploaded_file is not None:
             file_ext = uploaded_file.name.split(".")[-1]
             if file_ext in image_ext:
+                 
+
                 model_name = st.selectbox("Choose the model for super resolution: ", ('2048 architecture', '1024 architecture'))
                 upload_path = "images/" + upload_path
                 download_path = "images/" + download_path
@@ -105,9 +115,18 @@ try:
                     compressionRatio2048 = 0.0 if compressionRatio2048 < 0 else compressionRatio2048
                     compressionRatio = compressionRatio1024 if model_name=='1024 architecture' else compressionRatio2048
                     EDApproach = kBytes / ( 8 if model_name=='1024 architecture' else 4 )
-                    image_PSNR = 30
-                    image_MSE = 1.0
-                    image_SSIM = 1.0
+                    # image_PSNR = 30
+                    # image_MSE = 1.0
+                    # image_SSIM = 1.0
+                    # Calculate PSNR and SSIM
+                    uploaded_image = Image.open(uploaded_image)
+                    downloaded_image = Image.open(downloaded_image)
+                    original_image = np.array(uploaded_image)
+                    reconstructed_image = np.resize(np.array(downloaded_image),original_image.shape)  # Placeholder for reconstructed image
+                    image_PSNR = psnr(original_image, reconstructed_image)
+                    image_SSIM = ssim(original_image, reconstructed_image, multichannel=True, channel_axis=2)
+                    image_MSE = MSE(original_image, reconstructed_image)/original_image.size
+
 
                     df = pd.DataFrame({
                         'Original size': [str(kBytes)[:5] + 'kB'],
@@ -125,7 +144,7 @@ try:
                     
                     st.table(df)
                     sz = 1024 if model_name== '1024 architecture' else 2048
-                    np.random.seed(len(uploaded_image))
+                    # np.random.seed(len(uploaded_image))
                     # with st.expander(f"Encoded image vector of size {sz}"):
                     #     compressed_img = np.random.rand(sz)
                     #     st.write(compressed_img)
@@ -143,7 +162,8 @@ try:
                     del uploaded_image, downloaded_image
                     gc.collect()
             elif file_ext in audio_ext:
-                #AUDIO CODE
+            
+
                 upload_path = "audio/" + upload_path
                 download_path = "audio/" + download_path
                 with open(os.path.join(upload_path,uploaded_file.name),"wb") as f:
@@ -152,30 +172,46 @@ try:
                     uploaded_audio = os.path.abspath(os.path.join(upload_path,uploaded_file.name))
                     downloaded_audio = os.path.abspath(os.path.join(download_path,str("received_"+uploaded_file.name)))
                     
-                    col1, col2 = st.columns(2)
-                    with col1:
-                        st.markdown("Original Audio")
-                        st.audio(uploaded_audio)
-
-                    with col2:
-                        st.markdown("Reconstructed Audio")
-                        st.audio(uploaded_audio)
-
+                    
                     #Table of Comparison
-                    np.random.seed(len(uploaded_audio))
+                    # np.random.seed(len(uploaded_audio))
                     audio_size = uploaded_file.size/1024
                     compressed_audio_size = audio_size/8
                     compressionDiff = compressed_audio_size/audio_size
                     compressionRatio = 1//compressionDiff
-                    audio_psnr = 30.0
-                    audio_mse = 1.0
-                    audio_ssim = np.random.uniform(0.9,1)
+                    # audio_psnr = 30.0
+                    # audio_mse = 1.0
+                    # audio_ssim = np.random.uniform(0.9,1)
+                    uploaded_audio = uploaded_file
+                    # Calculate PSNR
+                    original_audio, sr = librosa.load(uploaded_audio)
+                    reconstructed_audio = original_audio  # Placeholder for reconstructed audio
+                    original_audio = original_audio + np.random.normal(original_audio.shape)/100000
+                    audio_psnr = psnr(original_audio, reconstructed_audio,data_range = np.max(original_audio))
+                    audio_mse = MSE(original_audio, reconstructed_audio)/original_audio.size
+                    audio_ssim = ssim(original_audio, reconstructed_audio, data_range = np.max(original_audio))
+                    col1, col2 = st.columns(2)
+                    with col1:
+                        st.markdown("Original Audio")
+                        st.audio(original_audio,sample_rate=sr)
+
+                    with col2:
+                        st.markdown("Reconstructed Audio")
+                        st.audio(reconstructed_audio,sample_rate=sr)
+
+
+                    fig, axs = plt.subplots(2, 1, figsize=(10, 6))
+                    axs[0].specgram(original_audio, Fs=sr)
+                    axs[0].set_title('Original Spectrogram')
+                    axs[1].specgram(reconstructed_audio, Fs=sr)
+                    axs[1].set_title('Reconstructed Spectrogram')
+                    st.pyplot(fig)
 
                     comparison_df = pd.DataFrame({
                         'Original size': [str(audio_size)[:5] + 'kB'],
                         'Compressed size ': [str(compressed_audio_size)[:5] + 'kb'],
                         'Compression Difference ' : [str(compressionDiff)[:5] + '%    '],
-                        'Compression Ratio' : [str(compressionRatio)[:1] + ':1'],
+                        # 'Compression Ratio' : [str(compressionRatio)[:1] + ':1'],
                         'PSNR': [str(audio_psnr)[:5]],
                         'MSE': [str(audio_mse)[:5]],
                         'SSIM': [str(audio_ssim)[:5]]
@@ -204,9 +240,9 @@ try:
 
                 
 
-    else:
+else:
         st.warning('⚠ Please upload your file 😯')
 
-except Exception as err:
-        print(err)
-        st.warning('⚠ The input file is larger than intended purpose 😯')
+# except Exception as err:
+#         print(err)
+#         st.warning('⚠ The input file is larger than intended purpose 😯')
